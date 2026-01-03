c2c9e4bb3d7a53ae60e8fd79cac1ef8c99f091eb
shrutisingh2176 <singhshruti2029@gmail.com>
2026-01-03 23:55:23 +0530

    Fetching API

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -12,7 +12,9 @@ const Body = () =>{
         const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7408388&lng=77.1125599&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        console.log(json);
+       
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
       return(
         <div className="Body" >
@@ -35,4 +37,18 @@ const Body = () =>{
         </div>
     )
     }
+
+
+
+type Restaurant = {
+    id: number;
+    name: string;
+    cuisine: string;
+};
+
+function setFilteredRestaurants(restaurants: Restaurant[]): void {
+    // implementation here
+}
+
+
 export default Body;
