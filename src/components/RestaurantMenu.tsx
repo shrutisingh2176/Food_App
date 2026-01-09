fc97dbd9001cd8c5cf0a69a67544397f5fcfeb73
shrutisingh2176 <singhshruti2029@gmail.com>
2026-01-09 23:59:54 +0530

    MENU-API

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -1,8 +1,11 @@
-import {useEffect} from "react";
-
+import {useState,useEffect} from "react";
+import Shimmer from "./Shimmer";
 
 
 const RestaurantMenu = () => {
+
+    const[resInfo, setResInfo]= useState<any>(null);
+
 
    useEffect(() => {
             fetchMenu();
@@ -11,24 +14,41 @@ const RestaurantMenu = () => {
     const fetchMenu = async () => {
     
         const data = await fetch(
-           "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6426028&lng=77.21921669999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+           "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7408388&lng=77.1125599&restaurantId=1304142&catalog_qa=undefined&submitAction=ENTER"
         );
 
      const json = await data.json();
         console.log(json);
+        setResInfo(json);
          };
-     
-        return (
-            <div className="menu">
-            <h1>Name of the Restaurant</h1>
+
+            if (resInfo === null) return <Shimmer />;
+
+
+        const {name, cuisines, costForTwoMessage} = resInfo?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info[0];  // change this according to data structure
+         
+
+         const {itemCards} = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card; // change this according to data structure  
+
+        return  (
+           
+           
+           <div className="menu">
+            <h1>{name}</h1>               // change this according to data structure
+            <h3>{cuisines.join(", ")}</h3>  
+            <h3>{costForTwoMessage}</h3>   // change this according to data structure
             <h2>Menu</h2>
             <ul>
-                <li>Paneer Butter Masala</li>
-                <li>Dal Makhani</li>
-                <li>Jeera Rice</li>
-                <li>Naan</li>
-                <li>Gulab Jamun</li>
+               {itemCards.map((item: any) => (
+                <li key={item.card.info.id}>
+                    {item.card.info.name} - {"Rs"} // change this according to data structure
+                    {item.card.info.price / 100 || item.card.info.defaultPrice / 100}   // change this according to data structure
+                </li>
+               ))}
             </ul>
             </div>
-        );}
-        export default RestaurantMenu;
+        );
+    };
+
+    
+            export default RestaurantMenu;
