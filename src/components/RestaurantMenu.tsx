f23f50fac666f1e8e429d1cc48c4e7084b708289
shrutisingh2176 <singhshruti2029@gmail.com>
2026-01-10 23:09:07 +0530

    Menu-Cards

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -1,21 +1,34 @@
 import {useState,useEffect} from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+
 
 
 const RestaurantMenu = () => {
 
     const[resInfo, setResInfo]= useState<any>(null);
-
+    
+    //const{resId}= useParams();
 
    useEffect(() => {
             fetchMenu();
         }, []);
 
-    const fetchMenu = async () => {
+
+    // const fetchMenu = async () => {
     
-        const data = await fetch(
-           "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7408388&lng=77.1125599&restaurantId=1304142&catalog_qa=undefined&submitAction=ENTER"
+    //     const data = await fetch(
+    //       MENU_API + resId  
+    //     );
+
+
+
+    const fetchMenu = async () => {
+         const data = await fetch(
+          " https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=24148 " 
         );
+
+
 
      const json = await data.json();
         console.log(json);
@@ -25,30 +38,46 @@ const RestaurantMenu = () => {
             if (resInfo === null) return <Shimmer />;
 
 
-        const {name, cuisines, costForTwoMessage} = resInfo?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info[0];  // change this according to data structure
+       // const {name, cuisines, costForTwoMessage} = resInfo?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info[0];  // change this according to data structure
          
 
-         const {itemCards} = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card; // change this according to data structure  
+        // const {itemCards} = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card; // change this according to data structure  
 
-        return  (
+    //     return  (
            
            
-           <div className="menu">
-            <h1>{name}</h1>               // change this according to data structure
-            <h3>{cuisines.join(", ")}</h3>  
-            <h3>{costForTwoMessage}</h3>   // change this according to data structure
+    //        <div className="menu">
+    //        <h1>{name}</h1>               // change this according to data structure
+    //         <h3>{cuisines.join(", ")}</h3>  
+    //         <h3>{costForTwoMessage}</h3>   // change this according to data structure
+    //         <h2>Menu</h2>
+    //         <ul>
+    //            {itemCards.map((item: any) => (
+    //             <li key={item.card.info.id}>
+    //                 {item.card.info.name} - {"Rs"} // change this according to data structure
+    //                 {item.card.info.price / 100 || item.card.info.defaultPrice / 100}   // change this according to data structure
+    //             </li>
+    //            ))}
+    //         </ul>
+    //         </div>
+    //     );
+    // };
+
+
+
+    return (
+        <div className="menu">
+            <h1>Name of the restaurant</h1>
+            <h3>Cuisines</h3>
+            <h3>Cost for two</h3>
             <h2>Menu</h2>
             <ul>
-               {itemCards.map((item: any) => (
-                <li key={item.card.info.id}>
-                    {item.card.info.name} - {"Rs"} // change this according to data structure
-                    {item.card.info.price / 100 || item.card.info.defaultPrice / 100}   // change this according to data structure
-                </li>
-               ))}
+                <li>Menu Item 1</li>
+                <li>Menu Item 2</li>
+                <li>Menu Item 3</li>        
             </ul>
             </div>
-        );
-    };
-
+    );
+}
     
             export default RestaurantMenu;
