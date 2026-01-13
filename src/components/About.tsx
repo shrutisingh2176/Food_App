35a218066b4bca686ba638b67e3116363ef1c81b
shrutisingh2176 <singhshruti2029@gmail.com>
2026-01-13 11:19:09 +0530

    Multiple-Childrens & Onclick-btn

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,30 @@
  import User from "./User";
  import UserClass from "./UserClass";
+ import { Component } from "react";
+ //import React from "react";
+
+    class About extends Component { // On place of Component we can also => React.Component
+        constructor(props: any) {
+            super(props);
+           // console.log("Parent Constructor");
+        }
+
+        componentDidMount(): void {
+          //  console.log("Parent Component Did Mount");
+        }
+
+        render() { 
+           // console.log("Parent Render");
+            return(
+                <div>
+                    <h1>About Us</h1>
+                    <h2> This is Namaste React Web Series </h2>
+                    <UserClass name={"First"} location={"Delhi (class)"}/>
+                     
+                </div>
+            );
+        };
+    }
+
  
- const About=() =>{
-    return(
-        <div>
-            <h1>About Us</h1>
-            <h2> This is Namaste React Web Series </h2>
-             <UserClass name={"Shruti (class)"} location={"Delhi (class)"}/>
-        </div>
-    )
- };
     export default About;
--- src/components/UserClass.tsx
+++ src/components/UserClass.tsx
@@ -13,26 +13,40 @@ interface UserState {
 class UserClass extends React.Component<UserProps, UserState> {
     constructor(props: any) {
         super(props);
-        this.state = {
-            count:0
-        };
+
+        // this.state = {
+        //     count:0
+        // };
+
+        //console.log(this.props.name + "Child Constructor");
+    }
+
+     componentDidMount(){
+       // console.log(this.props.name + "Child Component Did Mount");
+        // API call
+
     }
 
     render() {
         const { name, location } = this.props;
-        const { count } = this.state;
+       // const { count } = this.state;
+       
+       // console.log( this.props.name + "Child Render");
+
         return (
         <div className ="user-card">
-            <h1>Count: {count}</h1>
-            <button onClick ={() => {
+            {/* <h1>Count: {count}</h1> */}
+            {/* <button onClick ={() => {
                
                this.setState({count: this.state.count +1
                    
                  });
-            }}>Count Increase</button>
+            }}>Count Increase</button> */}
+
             <h2> Name: {name} </h2>
             <h3>Location: {location}</h3>
             <h4> Contact: @shruti123</h4>
+
         </div>  
         );
     }
